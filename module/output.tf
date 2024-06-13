2506cae807d68d85f67df95ebd1b131ad2f01447
Pradeep Reddy <pradeep.reddy@ryvalx.com>
2024-06-13 13:17:09 +0530

    changes made in tf files

--- module/output.tf
+++ module/output.tf
@@ -5,10 +5,9 @@ output "vpc_name" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-output "region" {   value = var.region } 
-output "public_subnet_ids" {   value = [module.vpc.public_subnet.*.ids] } 
-output "route_table_ids" {   value = [module.vpc.route_table_id.rt_id] } 
-output "internet_gateway_id" {
-  value = module.vpc.igw.id
+
+output "region" {
+  value = var.region
+  
 }
 
