95105f0176104b93213e78616be7d65e80a3f4dd
Pradeep Reddy <pradeep.reddy@ryvalx.com>
2024-06-13 12:47:11 +0530

    terraform output.tf updated

--- module/output.tf
+++ module/output.tf
@@ -5,3 +5,10 @@ output "vpc_name" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+output "region" {   value = var.region } 
+output "public_subnet_ids" {   value = module.public_subnet_ids.public[*].id } 
+output "route_table_ids" {   value = [module.route_table_id.public.id] } 
+output "internet_gateway_id" {
+  value = module.internet_gateway_id.vpc_id
+}
+
